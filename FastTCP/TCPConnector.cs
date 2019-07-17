cca68aeec87d8aea5d2d9d334b456d435576826c
Админ <@->
2019-07-17 17:40:57 +0300

    update

--- FastTCP/TCPConnector.cs
+++ FastTCP/TCPConnector.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FastTCP
@@ -27,22 +28,75 @@ namespace FastTCP
             if (Conntype == ConnectionType.Server)
             {
                 tcpListener = new TcpListener(iPAddress, Port);
-                await Task.Run(() => tcpListener.Start());
-                await Task.Delay(500);
-                tcpClient = await tcpListener.AcceptTcpClientAsync();
+                listenThread = new Thread(tcpListener.Start);
+                listenThread.Start();
+                Connect();
             }
             else
             {
                 tcpClient = new TcpClient();
                 await tcpClient.ConnectAsync(iPAddress, Port);
+                PeerConnected?.Invoke(null, null);
             }
+        }
+        public async void Connect()
+        {
+            await Task.Delay(500);
+            tcpClient = await tcpListener.AcceptTcpClientAsync();
+            listenThread.Abort();
+            PeerConnected?.Invoke(null, null);
         }
         private ConnectionType Conntype;
         private TcpListener tcpListener;
         private TcpClient tcpClient;
-        private Task listenTask;
+        private Thread listenThread;
         private IPAddress iPAddress;
         private int Port;
+        private NetworkStream netStream;
+        public event EventHandler PeerConnected;
+        public event EventHandler PeerDisconnected;
+        public string ClientIp => ((IPEndPoint)tcpClient.Client.RemoteEndPoint).Address.ToString();
+        public bool IsConnected
+        {
+            get
+            {
+                if (tcpClient == null) return false;
+                else return tcpClient.Connected;
+            }
+        }
+        public void OpenStream() => netStream = tcpClient.GetStream();
+        public async void SendDataAsync(byte[] data)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    OpenStream();
+                    netStream.Write(data, 0, data.Length);
+                }
+                catch(Exception ex)
+                {
+                    PeerDisconnected?.Invoke(ex.ToString(), EventArgs.Empty);
+                }
+            });
+        }
+        public async void SendDataAsync(byte[] data, byte[] EndBytes)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    OpenStream();
+                    netStream.Write(data, 0, data.Length);
+                    netStream.Write(EndBytes, 0, EndBytes.Length);
+                    netStream.Flush();
+                }
+                catch (Exception ex)
+                {
+                    PeerDisconnected?.Invoke(ex.ToString(), EventArgs.Empty);
+                }
+            });
+        }
 
     }
 }
