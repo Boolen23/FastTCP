a9da6ef6bb05dbb4c30e94ca0ea9763a5daa3d83
Админ <@->
2019-07-17 16:35:11 +0300

    Добавьте файлы проекта.

---
+++ FastTCP/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("FastTCP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FastTCP")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("31e92176-5180-4ae4-b5fd-52a89da11a8d")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FastTCP/TCPConnector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastTCP
+{
+    public class TCPConnector
+    {
+        public TCPConnector(ConnectionType type, int port)
+        {
+            Conntype = type;
+            Port = port;
+            iPAddress = IPAddress.Any;
+        }
+        public TCPConnector(ConnectionType type, int port, string iP)
+        {
+            Conntype = type;
+            iPAddress = IPAddress.Parse(iP);
+            Port = port;
+        }
+        public async void Initialize()
+        {
+            if (Conntype == ConnectionType.Server)
+            {
+                tcpListener = new TcpListener(iPAddress, Port);
+                await Task.Run(() => tcpListener.Start());
+                await Task.Delay(500);
+                tcpClient = await tcpListener.AcceptTcpClientAsync();
+            }
+            else
+            {
+                tcpClient = new TcpClient();
+                await tcpClient.ConnectAsync(iPAddress, Port);
+            }
+        }
+        private ConnectionType Conntype;
+        private TcpListener tcpListener;
+        private TcpClient tcpClient;
+        private Task listenTask;
+        private IPAddress iPAddress;
+        private int Port;
+
+    }
+}
